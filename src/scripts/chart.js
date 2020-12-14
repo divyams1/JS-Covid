76e6bcf3d52abfa71f17c90ed1c01333171205ab
divyams1 <divyamsat@gmail.com>
2020-12-13 21:43:09 -0500

    added view other state

--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -1,8 +1,12 @@
-import { dropdown } from './dropdown.js';
+// import { dropdown } from './dropdown.js';
+
+import { timeDropdown } from "./dropdown";
 
 
 
 export const chart = (stateName) => {
+              d3.select('#state-message')
+                .remove()
               var xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = function () {
                     if (this.readyState == 4 && this.status == 200) {
@@ -17,8 +21,8 @@ export const chart = (stateName) => {
                         const updateChart = (selectedGroup) => {
                            {
                                 const margin = ({ top: 10, right: 0, left: 20, bottom:15})
-                                const height =300  
-                                const width = 300
+                                const height =350
+                                const width = 350
                                 const stateNumbers = data.map( state => { return state['state']})
                                 const stateNumber = stateNumbers.indexOf(selectedGroup)
                                 const singleStateInfo = Object.values( JSON.parse(xhttp.response) ) 
@@ -31,6 +35,9 @@ export const chart = (stateName) => {
                                     .attr("viewBox", [0,0, width, height])
                                     .attr('height', height)
                                     .attr('width', width)
+                                    .attr('font-family' , 'sans-serif')
+                                    .attr('font-size', 10)
+                                    .attr('text-anchor', 'end')
                                     .attr('id', 'supplement-chart');
                                 const y2 = d3.scaleLinear()
                                     .domain([0, d3.max(dataToDisplay, d => d[1] )])
@@ -43,11 +50,14 @@ export const chart = (stateName) => {
                                 svg.append('g')
                                     .attr('transform', `translate(0,${height-margin.bottom})`)
                                     .call(d3.axisBottom(x2))
+                                svg.append('g')
+                                    .attr('transform', `translate(${margin.left},0)`)
+                                    .attr(d3.axisLeft(y2))
                                 const xAxis2 = g => g
                                     .attr('transform', `translate( 0, ${height- margin.bottom})`)
                                     .call(d3.axisBottom(x2).tickSizeOuter(0))
                                 svg.append('g')
-                                    .attr('fill', 'steelblue')
+                                    .attr('fill', 'red')
                                   .selectAll('rect')
                                   .data(dataToDisplay)
                                   .join('rect')
@@ -55,31 +65,35 @@ export const chart = (stateName) => {
                                     .attr('y', d => y2(d[1]))
                                     .attr('height', d => y2(0) - y2(d[1]))
                                     .attr('width', x2.bandwidth())
-                                    .append('text')
-                                        .attr('x', -(height / 2) - margin.bottom)
-                                        .attr('y', margin.bottom / 2.4)
-                                        .style('fill', 'black')
-                                        .style('text-anchor', 'middle')
-                                        .attr('transform', 'rotate(-90)')
-                                        .text(d => d[1])
+
+                                // svg.append('text')
+                                //     .attr('fill', 'white')
+                                //     .attr('x', d=> x2(d[1]-3))
+                                //     .attr('y', y.bandwidth()/2)
+                                //     .attr('dy', '0.35em')
+                                //     .text( d => d[1])
                                 
                                     
-                                      
+                                const yAxis2 = g => g 
+                                    .attr('transform', `translate(${margin.left}, 0)`)
+                                    .call(d3.axisLeft(y2).ticks(null, ""))
+                                    .call(g => g.select(".domain").remove()) 
                                     
                                 svg.append('g')
                                     .call(xAxis2)
                                 
-                                // svg.append('g')
-                                //     .call(yAxis2)
+                                svg.append('g')
+                                    .call(yAxis2)
                                 
                                 // svg.call(yTitle2)
 
+                                 
                                 
-                                dropdown();
                             }
                         }
 
                          updateChart(stateName)
+                        
                     }
                 };
                 xhttp.open("GET", 'https://api.covidtracking.com/v1/states/current.json', true);
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,6 +1,7 @@
  
 var d3 = require("d3")
 import { chart} from './chart';
+import { stateDropdown, dropdownHeatMap, timeDropdown } from './dropdown'
 
 
 
@@ -12,7 +13,7 @@ const map = () => {
      
         if (this.readyState == 4 && this.status == 200) {
            
-           
+             
              const stateInfo = Object.values( JSON.parse(this.response) ) 
               let data = stateInfo.map( state  => {
                             return [  state["state"] ,  state["total"], (state["deathConfirmed"]/state["positive"]) ]      
@@ -20,20 +21,20 @@ const map = () => {
              let colorData = stateInfo.map( state  => {
                   return  (state["death"]/state["positive"])          
             })
-           
+            
               data.splice(3,1)
               data.splice(11,1)
               data.splice(25,1)
               data.splice(47,1)
               data.splice(39,1)
         
-               colorData.splice(3,1)
-             colorData.splice(11,1)
-               colorData.splice(25,1)
-               colorData.splice(47,1)
-              colorData.splice(39,1)
-              
-            
+            colorData.splice(3,1)
+            colorData.splice(11,1)
+            colorData.splice(25,1)
+            colorData.splice(47,1)
+            colorData.splice(39,1)
+            dropdownHeatMap(stateInfo)
+            stateDropdown(data);
              const myColor = d3.scaleLinear()
                          .range(["#F08080", "#8B0000"])
                         .domain([Math.min(...colorData), Math.max(...colorData)])
@@ -51,9 +52,11 @@ const map = () => {
                         .style('position' ,'absolute')
                         .style('z-index', 10)
                         .style('visibility', 'hidden')
-                        .style('background', '#000')
+                        .style('background', '#228B22')
                         .text('a simple tool tip')
                         .style('color', 'white')
+                        .style('height', '30px')
+                        .style('border', '1px solid black')
             d3.select('body')
                     .selectAll('.state-path')
                     .append('div')
@@ -71,13 +74,23 @@ const map = () => {
                     .on('click' , (e) => {
                         const stateName= e.currentTarget.id 
                         chart(stateName)
-                        
+                        timeDropdown(stateName)
                     })
                     .style('fill', function(d) { return myColor(state[2])})
             })
             d3.selectAll('title')
                 .text('')
+     
+            // const xScale = d3.scaleBand()
+            //     .domain([0,1])
+            //     .range([0,width]);
+
+            // const yScale = d3.scaleLinear()
+            //     .domain(domain)
+            //     .range(height, 0)
            
+          
+
     }
 }
    xhttp.open("GET", ' https://api.covidtracking.com/v1/states/current.json', true);
@@ -87,3 +100,70 @@ const map = () => {
 }
 export default map;
 
+
+
+
+
+
+
+
+
+    //    const legendHeight = 500
+    //         const legendWidth = 500
+    //         const x1 = 200 
+    //         const barWidth = 300
+    //         const y1 = 150
+    //         const barHeight = 100
+    //         const numberHues = 35 
+
+    //         const idGradient = "legendGradient"
+    //        const svgForLegend = d3.select('#theBar').append("svg")
+    //         .attr('width', legendWidth)
+    //         .attr('height', legendHeight)
+
+    //         svgForLegend.append('g')
+    //             .append('defs')
+    //             .append('linearGradient')
+    //                 .attr('id', idGradient)
+    //                 .attr('x1', '0%')
+    //                 .attr('x2', '100%')
+    //                 .attr('y1', '0%')
+    //                 .attr('y2', '0%')
+
+    //     svgForLegend.append('rect')
+    //         .attr('fill', `url(#${idGradient})`)
+    //         .attr('x', x1)
+    //         .attr('y', y1)
+    //         .attr('width', legendWidth)
+    //         .attr('height', legendHeight)
+    //         .attr('rx', 20)
+    //         .attr('ry', 20)
+    //     const textY = y1 + barHeight/2 + 15;
+    //     svgForLegend.append('text')
+    //         .attr('class', 'legendText')
+    //         .attr('text-anchor', 'middle')
+    //         .attr('x', x1-25)
+    //         .attr('y', textY)
+    //         .attr('dy', 0)
+    //         .text(0)
+
+    // svgForLegend.append('text')
+    //         .attr("class","legendText")
+    //         .attr("text-anchor", "left")
+    //         .attr("x",x1 + barWidth + 15)
+    //         .attr("y",textY)
+    //         .attr("dy",0)
+    //         .text(numberHues + "+");
+        
+    // const hueStart = 160
+    // const hueEnd = 0
+
+    // const opacityStart = 0.3 
+    // const opacityEnd = 1.0
+
+    // const theHue, rgbString, opacity, p; 
+
+    // const deltaPercent = 1/(numberHues-1)
+
+    // const deltaHue = ( hueEnd - hueStart)/(numberHues-1)
+    // const deltaOpacity = (opacityEnd - opacityStart)/(numberHues-1)
